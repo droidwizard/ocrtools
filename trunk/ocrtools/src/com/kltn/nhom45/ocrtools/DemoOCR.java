5aa8bdbc28ea8aa2727989030917f1b0767b5ed2
trieuldt@gmail.com <trieuldt@gmail.com@c53663c1-9987-b6be-09b1-f16de9c03757>
2012-10-18 17:14:57 +0000

    BaseOCR chứa xử lý nhận dạng text chung cho 2 lớp DemoOCR, InputImage InitData khai báo trainneddata,tạo folder, file ban đầu

---
+++ trunk/ocrtools/src/com/kltn/nhom45/ocrtools/DemoOCR.java
@@ -0,0 +1,106 @@
+package com.kltn.nhom45.ocrtools;
+
+import java.io.File;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class DemoOCR extends BaseOCR implements OnClickListener {
+	private static final String TAG = "DemoOCR.java";
+
+	public static final String DATA_PATH = Environment
+			.getExternalStorageDirectory().toString() + "/OCRTools/";
+
+	public static final String lang = "eng";
+
+	protected Button _button;
+	protected EditText _field;
+	protected String _path;
+	protected boolean _taken;
+	private String text;
+	Bitmap bitmap;
+	protected static final String PHOTO_TAKEN = "photo_taken";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.demoocr);
+
+		_field = (EditText) findViewById(R.id.editText1);
+		_button = (Button) findViewById(R.id.btn_demoocr_takephoto);
+		_button.setOnClickListener(DemoOCR.this);
+
+		_path = DATA_PATH + "/ocr.jpg";
+	}
+
+	public void onClick(View v) {
+		if (v == _button) {
+			Log.v(TAG, "Starting Camera app");
+			startCameraActivity();
+		}
+
+	}
+
+	protected void startCameraActivity() {
+		File file = new File(_path);
+		Uri outputFileUri = Uri.fromFile(file);
+
+		final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+		startActivityForResult(intent, 0);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		Log.i(TAG, "resultCode: " + resultCode);
+
+		if (resultCode == -1) {
+			onPhotoTaken();
+		} else {
+			Log.v(TAG, "User cancelled");
+		}
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putBoolean(DemoOCR.PHOTO_TAKEN, _taken);
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		Log.i(TAG, "onRestoreInstanceState()");
+		if (savedInstanceState.getBoolean(DemoOCR.PHOTO_TAKEN)) {
+			onPhotoTaken();
+		}
+	}
+
+	protected void onPhotoTaken() {
+		_taken = true;
+		
+		BitmapFactory.Options options = new BitmapFactory.Options();
+		options.inSampleSize = 4;
+
+		bitmap = BitmapFactory.decodeFile(_path, options);
+
+		text = onHanldeOCR(bitmap);
+
+		if (text.length() != 0) {
+			_field.setText(_field.getText().toString().length() == 0 ? text
+					: _field.getText() + " " + text);
+			_field.setSelection(_field.getText().toString().length());
+		}
+	}
+
+}
--- trunk/ocrtools/src/com/kltn/nhom45/ocrtools/MainActivity.java
+++ trunk/ocrtools/src/com/kltn/nhom45/ocrtools/MainActivity.java
@@ -1,20 +1,69 @@
 package com.kltn.nhom45.ocrtools;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+
+public class MainActivity extends Activity implements OnClickListener{
+	//private static final String TAG = "MainActivity.java";
+	
+	private Context context=MainActivity.this;
+	private Button btn_main_DemoOCR;
+	private Button btn_main_InputImage;
+	private ProgressDialog progressDialog;
+	private InitData mInitData;
+	
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_main_DemoOCR=(Button) findViewById(R.id.btn_main_1);
+        btn_main_DemoOCR.setOnClickListener(MainActivity.this);
+        btn_main_InputImage=(Button) findViewById(R.id.btn_main_2);
+        btn_main_InputImage.setOnClickListener((OnClickListener) context);
+        
+        
     }
 
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	progressDialog = ProgressDialog.show(context, "Please wait for a second",
+				"Processing");
+    	mInitData=new InitData(context,progressDialog);
+
+    	mInitData.start();
+
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	public void onClick(View v) {
+		if (v==btn_main_DemoOCR){
+			Intent i=new Intent(context, DemoOCR.class);
+			startActivity(i);
+		}
+		if (v==btn_main_InputImage){
+			Intent i=new Intent(context, InputImage.class);
+			startActivity(i);
+		}
+	}
+	
+	
+
+    
 }
