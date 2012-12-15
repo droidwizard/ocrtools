8788f36e2194885c5b73a9cd6a337aed1f507466
trieuldt <trieuldt@gmail.com>
2012-12-15 11:07:54 +0000

    rework code

--- ocrtools/src/com/uit/ocr/utils/Consts.java
+++ ocrtools/src/com/uit/ocr/utils/Consts.java
@@ -3,5 +3,8 @@ package com.uit.ocr.utils;
 public class Consts {
 
 	public static String SETTINGS_TITLE_EVENTS = "titleEvents";
+	public static String DEFAULT_EVENT_1 = "Hội họp";
+	public static String DEFAULT_EVENT_2 = "Tiệc cưới";
+	public static String DEFAULT_EVENT_3 = "Sinh nhật";
 
 }
