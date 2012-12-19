beb9e6d61b2423b9a966499b80062a6d266679e9
nam.hd23@gmail.com <nam.hd23@gmail.com@c53663c1-9987-b6be-09b1-f16de9c03757>
2012-12-19 14:50:13 +0000

    xong dịch, lỗi hiển thị một số tiếng (Trung Quốc, Hàn Quốc, Nhật...), còn lại xuất file và fix hiển thị

--- trunk/ocrtools/src/com/uit/ocr/Settings.java
+++ trunk/ocrtools/src/com/uit/ocr/Settings.java
@@ -22,6 +22,39 @@ import com.uit.ocr.utils.Consts;
 
 
 public class Settings extends Activity implements OnCheckedChangeListener,OnItemSelectedListener {
+	
+	private final String DATA_ENGLISH = "eng";
+	private final String DATA_GERMAN = "deu";
+	private final String DATA_DUTCH = "nld";
+	private final String DATA_KOREAN = "kor";
+	private final String DATA_INDONESIA = "ind";
+	private final String DATA_ITALIA = "ita";
+	private final String DATA_MALAYSIA = "msa";
+	private final String DATA_RUSSIA = "rus";
+	private final String DATA_JAPAN = "jpn";
+	private final String DATA_FRANCE = "fra";
+	private final String DATA_SPAIN = "spa";
+	private final String DATA_THAILAND = "tha";
+	private final String DATA_CHINA = "chi_tra";
+	private final String DATA_VIETNAM = "vie";
+	
+	private final String LANG_ENGLISH = "Anh";
+	private final String LANG_GERMAN = "Đức";
+	private final String LANG_DUTCH = "Hà Lan";
+	private final String LANG_KOREAN = "Hàn Quốc";
+	private final String LANG_INDONESIA = "Indonesia";
+	private final String LANG_ITALIA = "Italia";
+	private final String LANG_MALAYSIA = "Malaysia";
+	private final String LANG_RUSSIA = "Nga";
+	private final String LANG_JAPAN = "Nhật Bản";
+	private final String LANG_FRANCE = "Pháp";
+	private final String LANG_SPAIN = "Tây Ban Nha";
+	private final String LANG_THAILAND = "Thái Lan";
+	private final String LANG_CHINA = "Trung Quốc";
+	private final String LANG_VIETNAM = "Việt Nam";
+	
+	private String language;
+	
 	CheckBox cbSettings2ColorImage, cbSettingsCharfilter;
 
 	private Context context = Settings.this;
@@ -121,13 +154,68 @@ public class Settings extends Activity implements OnCheckedChangeListener,OnItem
 	};
 	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
 		// TODO Auto-generated method stub
-		if(parent.getItemAtPosition(pos).toString().equals("Việt Nam"))
+		language = parent.getItemAtPosition(pos).toString();
+		if(language.equals(LANG_VIETNAM))
 		{
 			MainActivity.flagLock = true;
+			BaseOCR.lang = DATA_VIETNAM;
+			Toast.makeText(getApplicationContext(), BaseOCR.lang, 200).show();
 		}
 		else
 		{
 			MainActivity.flagLock = false;
+			if(language.equals(LANG_GERMAN))
+			{
+				BaseOCR.lang = DATA_GERMAN;
+			}
+			else if(language.equals(LANG_DUTCH))
+			{
+				BaseOCR.lang = DATA_DUTCH;
+			}
+			else if(language.equals(LANG_KOREAN))
+			{
+				BaseOCR.lang = DATA_KOREAN;
+			}
+			else if(language.equals(LANG_INDONESIA))
+			{
+				BaseOCR.lang = DATA_INDONESIA;
+			}
+			else if(language.equals(LANG_ITALIA))
+			{
+				BaseOCR.lang = DATA_ITALIA;
+			}
+			else if(language.equals(LANG_MALAYSIA))
+			{
+				BaseOCR.lang = DATA_MALAYSIA;
+			}
+			else if(language.equals(LANG_RUSSIA))
+			{
+				BaseOCR.lang = DATA_RUSSIA;
+			}
+			else if(language.equals(LANG_JAPAN))
+			{
+				BaseOCR.lang = DATA_JAPAN;
+			}
+			else if(language.equals(LANG_FRANCE))
+			{
+				BaseOCR.lang = DATA_FRANCE;
+			}
+			else if(language.equals(LANG_SPAIN))
+			{
+				BaseOCR.lang = DATA_SPAIN;
+			}
+			else if(language.equals(LANG_THAILAND))
+			{
+				BaseOCR.lang = DATA_THAILAND;
+			}
+			else if(language.equals(LANG_CHINA))
+			{
+				BaseOCR.lang = DATA_CHINA;
+			}
+			else if(language.equals(LANG_ENGLISH))
+			{
+				BaseOCR.lang = DATA_ENGLISH;
+			}
 		}
 		
 	}
