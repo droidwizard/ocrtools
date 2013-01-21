de5a9f03d332f682183ba320b5cfe3c519a2833b
nam.hd23 <nam.hd23@gmail.com>
2013-01-21 13:57:09 +0000

    edit translate

--- ocrtools/src/com/uit/ocr/utils/JSONfunctions.java
+++ ocrtools/src/com/uit/ocr/utils/JSONfunctions.java
@@ -1,8 +1,11 @@
 package com.uit.ocr.utils;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -11,20 +14,16 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import android.util.Log;
 
 public class JSONfunctions {
-	private static final String TAG = JSONfunctions.class.getSimpleName();
-	private static JSONfunctions mJson=new JSONfunctions();
-
-	public static JSONfunctions getInstance(){
-		return mJson;
-	}
-
-	public JSONObject getJSONfromURL(String url) {
+		
+	public static JSONObject getJSONfromURL(String url, String encoding) {
 		InputStream is = null;
 		String result = "";
 		JSONObject jArray = null;
+
 		// http post
 		try {
 			HttpClient httpclient = new DefaultHttpClient();
@@ -35,12 +34,13 @@ public class JSONfunctions {
 			is = entity.getContent();
 
 		} catch (Exception e) {
-			Log.e(TAG, "Error in http connection " + e.toString());
+			Log.e("log_tag", "Error in http connection " + e.toString());
 		}
 
 		try {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					is, "iso-8859-1"), 8);
+					is, encoding), 8192);
+
 			StringBuilder sb = new StringBuilder();
 			String line = null;
 			while ((line = reader.readLine()) != null) {
@@ -48,16 +48,42 @@ public class JSONfunctions {
 			}
 			is.close();
 			result = sb.toString();
+			Log.d("Test result", result);
 		} catch (Exception e) {
-			Log.e(TAG, "Error converting result " + e.toString());
+			Log.e("log_tag", "Error converting result " + e.toString());
 		}
 
 		try {
 
 			jArray = new JSONObject(result);
 		} catch (JSONException e) {
-			Log.e(TAG, "Error parsing data " + e.toString());
+			Log.e("log_tag", "Error parsing data " + e.toString());
 		}
+
 		return jArray;
+	}
+
+	public static String msTranslate(String urlMSTrans) throws IOException {
+		StringBuilder response = new StringBuilder();
+		URL url = new URL(urlMSTrans);
+
+		HttpURLConnection httpconn = (HttpURLConnection) url.openConnection();
+
+		if (httpconn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					httpconn.getInputStream()), 8192);
+			String strLine = null;
+			while ((strLine = input.readLine()) != null) {
+				response.append(strLine);
+			}
+			input.close();
+		}
+
+		String result = response.toString();
+		int start = result.indexOf("<div dir=\"ltr\" class=\"t0\">") + 26;
+		int end = result.indexOf("</div>", start);
+		result = result.substring(start, end);
+		
+		return result;
 	}
 }
